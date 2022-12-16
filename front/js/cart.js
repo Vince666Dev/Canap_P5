a1b93eeaeba655db1eaf144ed1d42961ad80805a
VincentValade <vincent.valade@live.fr>
2022-12-16 17:36:03 +0100

    delete article and color ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -17,25 +17,28 @@ fetch("http://localhost:3000/api/products/")
   .then((response) =>  response.json())
   .then((response) => {
     showCart(response);
+    // deleteProduct(response);
 })
 .catch((error) => alert("Une erreur est survenue" + error));
 
 function showCart (productData) {
-    
-    const injectCartInHtml = document.getElementById("cart__items");
+    // let totalItemsQuantity = 0;
+    // let totalCartPrice = 0;
+    const itemBloc = document.getElementById("cart__items");
     const userCart = JSON.parse(window.localStorage.getItem("userCart"));
     
     if (userCart === null){
         alert("Aucun article dans le panier")
     }else{
         
+        // boucle pour ajouter noeud HTML des articles au panier avec les spécificités des articles.
         for(index = 0; index < userCart.length; index++ ) {
             
-            const findProduct = productData.find((element) => element._id === userCart[index].id);    
+            let findProduct = productData.find((element) => element._id === userCart[index].id);   
             const totalProductPrice = findProduct.price * userCart[index].quantity;  
           
-            let cartNode = "";
-            cartNode +=
+            let itemNode = "";
+            itemNode +=
             `<article class="cart__item" data-id="${userCart[index].id}" data-color="${userCart[index].color}">
                 <div class="cart__item__img">
                     <img src="${userCart[index].image}" alt="${userCart[index].altText}">
@@ -57,21 +60,137 @@ function showCart (productData) {
                     </div>
                 </div>
             </article>`;
-            injectCartInHtml.innerHTML += cartNode;
-            
-            // let totalItemsQuantity = 0;
-            // let totalCartPrice = 0;
-            // totalCartPrice += parseInt(findProduct.price * userCart[index].quantity);
-            // totalItemsQuantity += parseInt(userCart[index].quantity);
-            // document.getElementById("totalQuantity").innerHTML = totalItemsQuantity;
-            // document.getElementById("totalPrice").innerHTML = totalCartPrice;
-            
-            // console.log(totalCartPrice)
-        }   
-           
-
+            itemBloc.innerHTML += itemNode;
+        }
+        
+        let totalItemsQuantity = 0;
+        let totalCartPrice = 0;
+        
+        // boucle pour calculer et afficher prix total et quantité totale au panier
+        for(index = 0; index < userCart.length; index++) {
+            const findProduct = productData.find((element) => element._id === userCart[index].id);
+            totalCartPrice += parseInt(findProduct.price * userCart[index].quantity);
+            totalItemsQuantity += parseInt(userCart[index].quantity);
+            document.getElementById("totalQuantity").innerHTML = totalItemsQuantity;
+            document.getElementById("totalPrice").innerHTML = totalCartPrice;
+        }
+        
+        
+        // boucle pour bouton supprimer du panier
+        for (index = 0; index < userCart.length; index ++) {
+            let foundProduct = userCart.find((element) => element.id === userCart[index].id && element.color === userCart[index].color);
+            const deleteBtn = document.querySelectorAll(".deleteItem");
+            console.log(userCart);
+            deleteBtn[index].addEventListener("click", () => {
+                deleteProduct(foundProduct);
+                console.log(foundProduct);
+                alert(foundProduct.name + " a bien été supprimé du panier.");
+                location.reload();
+            });    
+        }
     }
 }
+
+// fonction pour supprimer l'article cliqué et mettre à jour le panier
+function deleteProduct (product) {
+    const userCart = JSON.parse(window.localStorage.getItem("userCart"));
+    userCart.splice(userCart.findIndex((v) => v.id === product.id && v.color === product.color), 1);
+    localStorage.setItem("userCart", JSON.stringify(userCart));
+}     
+
+
+
+// function ne marche pas
+// function deleteItem (item) {
+//     const btn = document.querySelector(".deleteItem");
+//     btn.addEventListener("click", () => deleteItem(item))
+//     console.log("item to delete", item)
+// }
+
+
+// const productDeleteBtn = document.getElementsByClassName("deleteItem");
+// productDeleteBtn.addEventListener("click", () => {
+//     deleteProduct(findProduct);
+//     alert(findProduct.name + "a été supprimé du panier.");
+//     showCart();
+// });
+
+// deleteItem();
+
+
+// function deleteProduct(productData) {
+//     userCart.splice(userCart.findIndex((v) => v.id === productData._id), 1);
+//     localStorage.setItem("userCart", JSON.stringify(userCart));
+//     showCart();
+// }
+
+// function deleteItem() {
+//     const deleteButtons = document.querySelectorAll(".deleteItem");
+//     deleteButtons.forEach((deleteButton) => {
+//         deleteButton.addEventListener("click", (event) => {
+//             event.preventDefault();
+//             let userCart = JSON.parse(window.localStorage.getItem("userCart"));
+//             const deleteId = event.target.getAttribute("data-id");
+//             const deleteColor = event.target.getAttribute("data-color");
+//             userCart = userCart.filter((element) => !(element._id == deleteId && element.color == deleteColor));
+//             deleteConfirm = window.confirm(
+//                 "Etes vous sûr de vouloir supprimer cet article ?"
+//             );
+//             if (deleteConfirm == true) {
+//                 localStorage.setItem("userCart", JSON.stringify(userCart));
+//                 alert("Article supprimé avec succès");
+//                 const card = deleteButton.closest(".cart__item");
+//                 card.remove();
+//                 reloadCart();
+//             } else {
+//                 alert("Article non supprimé");
+//             }
+
+//             const deleteKanap = JSON.parse(localStorage.getItem("userCart"));
+
+//             if (deleteKanap.length === 0) {
+//                 localStorage.removeItem("userCart");
+//                 alert('Panier vide, retour à l\'accueil.');
+//                 window.location.href = "index.html";
+
+//             }
+//         });
+//     });
+// }
+
+// function reloadCart() {
+
+//     const userCart = JSON.parse(window.localStorage.getItem("userCart"));
+
+//     let totalItemsQuantity = 0;
+//     let totalCartPrice = 0;
+
+//     for (index = 0; index < userCart.length; index++) {
+//         const findProduct = userCart.find((element) => element.id === userCart[index].id);
+//         totalItemsQuantity += parseInt(userCart[index].quantity);
+//         totalCartPrice += parseInt(findProduct.price * userCart[index].quantity);
+//         // console.log(parseInt(findProduct.price * userCart[index].quantity));
+//         // document.getElementById("prix_" + i).innerHTML = findProduct.price * userCart[i].quantity + " €";
+//     }
+
+//     document.getElementById("totalQuantity").innerHTML = totalItemsQuantity;
+//     document.getElementById("totalPrice").innerHTML = totalCartPrice;
+// }
+
+
+        
+// function removeFromuserCart(product) {
+//     let userCart = JSON.parse(window.localStorage.getItem("userCart"));
+//     userCart = userCart.filter(p => p.id != product.id);
+//     localStorage.setItem("userCart", JSON.stringify(userCart));
+    
+// }        
+     
+   
+
+        
+           
+
 
 
 // CODE AMI MAIS NE MARCHE PAS
